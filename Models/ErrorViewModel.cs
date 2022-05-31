c6c93e29b645e561d33aacf48534d7302d807746
Andrei Jaume <andrei.jaumewillenska@utexas.edu>
2022-05-31 00:11:02 -0500

    add brand new solution for the JigsawPuzzleSolver project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JigsawPuzzleSolver.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
